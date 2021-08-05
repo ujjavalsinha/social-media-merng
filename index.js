1d805ede239436e3c6c9354b2941c03bf8a536d7
Ujjaval Sinha <39913625+ujjavalsinha@users.noreply.github.com>
2021-08-05 22:19:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const {ApolloServer, PubSub} = require('apollo-server')
+
+const mongoose = require('mongoose')
+
+const { MONGODB } = require('./config.js')
+
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
+const pubsub = new PubSub();
+
+const server  = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context : ({req}) => ({req, pubsub})
+});
+
+
+mongoose.connect(MONGODB, { useNewUrlParser : true})
+    .then(() => {
+        console.log("Mongo DB Connected")
+        return server.listen({port : 5000})
+    })
+    .then(res => {
+        console.log(`Server running at ${res.url}`)
+    })
+
+//Apollo server uses Express behind the scenes to create server and create routes
